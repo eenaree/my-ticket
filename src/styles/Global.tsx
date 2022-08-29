147d725c2699cb1251d4ccab9ad80bacae4b88bb
eenaree <narilee91@gmail.com>
2022-08-29 10:49:38 +0900

    Feat: web font UtoBalsamTint, Pretendard 추가 #1

--- src/styles/Global.tsx
+++ src/styles/Global.tsx
@@ -1,6 +1,7 @@
 import { Global } from '@emotion/react';
+import fontFace from './fontFace';
 import reset from './reset';
 
 export default function GlobalStyles() {
-  return <Global styles={reset} />;
+  return <Global styles={[reset, fontFace]} />;
 }
---
+++ src/styles/fontFace.ts
@@ -0,0 +1,66 @@
+import { css } from '@emotion/react';
+
+const fontFace = css`
+  @font-face {
+    font-family: 'UtoBalsamTint';
+    font-weight: normal;
+    font-style: normal;
+    src: url('https://cdn.jsdelivr.net/gh/webfontworld/utoBalsamtint/UtoBalsamTint.eot');
+    src: url('https://cdn.jsdelivr.net/gh/webfontworld/utoBalsamtint/UtoBalsamTint.eot?#iefix')
+        format('embedded-opentype'),
+      url('https://cdn.jsdelivr.net/gh/webfontworld/utoBalsamtint/UtoBalsamTint.woff2')
+        format('woff2'),
+      url('https://cdn.jsdelivr.net/gh/webfontworld/utoBalsamtint/UtoBalsamTint.woff')
+        format('woff'),
+      url('https://cdn.jsdelivr.net/gh/webfontworld/utoBalsamtint/UtoBalsamTint.ttf')
+        format('truetype');
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 200;
+    font-style: normal;
+    src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-ExtraLight.eot');
+    src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-ExtraLight.eot?#iefix')
+        format('embedded-opentype'),
+      url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-ExtraLight.woff2')
+        format('woff2'),
+      url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-ExtraLight.woff')
+        format('woff'),
+      url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-ExtraLight.ttf')
+        format('truetype');
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 400;
+    font-style: normal;
+    src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Regular.eot');
+    src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Regular.eot?#iefix')
+        format('embedded-opentype'),
+      url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Regular.woff2')
+        format('woff2'),
+      url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Regular.woff')
+        format('woff'),
+      url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Regular.ttf')
+        format('truetype');
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 600;
+    font-style: normal;
+    src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-SemiBold.eot');
+    src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-SemiBold.eot?#iefix')
+        format('embedded-opentype'),
+      url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-SemiBold.woff2')
+        format('woff2'),
+      url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-SemiBold.woff')
+        format('woff'),
+      url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-SemiBold.ttf')
+        format('truetype');
+    font-display: swap;
+  }
+`;
+
+export default fontFace;
