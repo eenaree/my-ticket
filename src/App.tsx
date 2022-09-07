ffd464b997956f55514df03fbbf8641e167f62df
eenaree <narilee91@gmail.com>
2022-09-07 20:04:55 +0900

    Feat: 인증 성공 후, redirect 페이지 추가 #5
    팝업창에서 sns 로그인 후, 리다이렉트될 페이지.
    이 페이지에서 다시 user data를 가져온 후, 팝업창이 닫히고 로그인이 완료됨.
    유저 정보는 추후 저장 예정.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Authenticated from '@pages/Authenticated';
 import Main from '@pages/Main';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route>
-          <Route path="/" element={<Main />} />
-        </Route>
+        <Route path="/" element={<Main />} />
+        <Route path="authenticated" element={<Authenticated />} />
       </Routes>
     </BrowserRouter>
   );
