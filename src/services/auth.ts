05acd959cec86dffaf3db0d51d323a41173bb20e
eenaree <narilee91@gmail.com>
2022-09-09 22:14:03 +0900

    Feat: `User`, `AuthResponse` 타입 추가

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,5 +1,12 @@
+import { User } from '@typings/db';
 import { authAPI } from '.';
 
+interface AuthResponse {
+  success: boolean;
+  message: string;
+  user: User;
+}
+
 export function login() {
-  return authAPI.get('/login');
+  return authAPI.get<AuthResponse>('/login');
 }
