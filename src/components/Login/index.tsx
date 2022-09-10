aa79e6779fe92f6b8f740d7b058da1e076d27dcd
eenaree <narilee91@gmail.com>
2022-09-10 21:48:48 +0900

    Refactor: sns 계정 로그인 버튼 리팩토링

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -6,6 +6,7 @@ import Button from '@components/common/Button';
 import Modal from '@components/common/Modal';
 import { baseURL } from '@services/index';
 import { useModalStore } from '@store/.';
+import { colors } from '@styles/theme';
 import { User } from '@typings/db';
 import { styles } from './styles';
 
@@ -14,8 +15,12 @@ interface Props {
   onSuccess(user: User): void;
 }
 
-interface LoginProps {
+interface ProviderProps {
+  Icon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
   onClickLogin: React.MouseEventHandler<HTMLButtonElement>;
+  text: string;
+  bgColor: string;
+  color: string;
 }
 
 interface MessageData {
@@ -23,7 +28,31 @@ interface MessageData {
   user: User | null;
 }
 
-type Provider = 'google' | 'kakao' | 'naver';
+type Provider = typeof providers[number]['provider'];
+
+const providers = [
+  {
+    provider: 'google',
+    Icon: GoogleLoginIcon,
+    text: '구글 계정으로 로그인',
+    bgColor: colors.gray[100],
+    color: colors.black,
+  },
+  {
+    provider: 'kakao',
+    Icon: KakaoLoginIcon,
+    text: '카카오 계정으로 로그인',
+    bgColor: '#fee500',
+    color: colors.black,
+  },
+  {
+    provider: 'naver',
+    Icon: NaverLoginIcon,
+    text: '네이버 아이디로 로그인',
+    bgColor: '#03c75a',
+    color: colors.white,
+  },
+] as const;
 
 export default function Login({ modal, onSuccess }: Props) {
   const closeModal = useModalStore(state => state.closeModal);
@@ -85,10 +114,17 @@ export default function Login({ modal, onSuccess }: Props) {
           <button css={styles.closeButton} onClick={closeModal} />
         </div>
         <div css={styles.modalBody}>
-          <ul>
-            <GoogleLogin onClickLogin={() => onClickLogin('google')} />
-            <KakaoLogin onClickLogin={() => onClickLogin('kakao')} />
-            <NaverLogin onClickLogin={() => onClickLogin('naver')} />
+          <ul css={styles.providerList}>
+            {providers.map(provider => (
+              <LoginProvider
+                key={provider.provider}
+                Icon={provider.Icon}
+                onClickLogin={() => onClickLogin(provider.provider)}
+                bgColor={provider.bgColor}
+                text={provider.text}
+                color={provider.color}
+              />
+            ))}
           </ul>
         </div>
       </section>
@@ -96,34 +132,21 @@ export default function Login({ modal, onSuccess }: Props) {
   );
 }
 
-function GoogleLogin({ onClickLogin }: LoginProps) {
+function LoginProvider({
+  Icon,
+  onClickLogin,
+  bgColor,
+  color,
+  text,
+}: ProviderProps) {
   return (
-    <li css={styles.googleLogin}>
-      <Button onClick={onClickLogin}>
-        <GoogleLoginIcon />
-        <span>구글 계정으로 로그인</span>
-      </Button>
-    </li>
-  );
-}
-
-function KakaoLogin({ onClickLogin }: LoginProps) {
-  return (
-    <li css={styles.kakaoLogin}>
-      <Button onClick={onClickLogin}>
-        <KakaoLoginIcon />
-        <span>카카오 계정으로 로그인</span>
-      </Button>
-    </li>
-  );
-}
-
-function NaverLogin({ onClickLogin }: LoginProps) {
-  return (
-    <li css={styles.naverLogin}>
-      <Button onClick={onClickLogin}>
-        <NaverLoginIcon />
-        <span>네이버 아이디로 로그인</span>
+    <li>
+      <Button
+        onClick={onClickLogin}
+        style={{ ['--provider-bg']: bgColor, ['--provider-color']: color }}
+      >
+        <Icon />
+        <span>{text}</span>
       </Button>
     </li>
   );
