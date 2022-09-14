ff9a159f50eac32f45f16302752a94b51de28a1c
Nari Lee <narilee91@gmail.com>
2022-09-14 22:10:45 +0900

    Refactor: router 파일 추가 (#18)
    router 파일 안에 라우터 로직만 별도 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Authenticated from '@pages/Authenticated';
-import Main from '@pages/Main';
+import GlobalStyles from '@styles/Global';
+import Router from './Router';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="authenticated" element={<Authenticated />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyles />
+      <Router />
+    </>
   );
 }
--- src/client.tsx
+++ src/client.tsx
@@ -1,13 +1,6 @@
 import { render } from 'react-dom';
-import GlobalStyles from '@styles/Global';
 import App from './App';
 
 const container = document.querySelector('#root');
 
-render(
-  <>
-    <GlobalStyles />
-    <App />
-  </>,
-  container
-);
+render(<App />, container);
