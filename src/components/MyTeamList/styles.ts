1f33f298aaee668952a1e3625da731676a5b4aee
Nari Lee <narilee91@gmail.com>
2022-09-28 15:19:11 +0900

    Design: MyTeamList 컴포넌트 일부 태그명, 리스트된 팀 뱃지 스타일 변경

--- src/components/MyTeamList/styles.ts
+++ src/components/MyTeamList/styles.ts
@@ -4,25 +4,31 @@ import { colors } from '@styles/theme';
 
 export const styles = {
   noneTeamList: css({
-    minHeight: 60,
-    padding: '0 1.25rem',
-  }),
-  teamListWrapper: css({
-    display: 'flex',
-    flexWrap: 'wrap',
+    height: 80,
+    padding: '1.25rem',
     backgroundColor: colors.gray[100],
     [mq('sm')]: {
-      padding: '1rem 0',
+      height: 100,
     },
   }),
   teamList: css({
+    display: 'flex',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    minHeight: 80,
+    backgroundColor: colors.gray[100],
+    [mq('sm')]: {
+      minHeight: 100,
+    },
+  }),
+  team: css({
     width: '20%',
     margin: '0.5rem 0',
     textAlign: 'center',
     button: {
       width: 60,
       height: 60,
-      border: `1px solid ${colors.gray[300]}`,
+      backgroundColor: colors.white,
       borderRadius: '50%',
     },
     [mq('sm')]: {
