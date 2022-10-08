49dd70e5822b89a26dd8b9d4372b648653f2c048
Nari Lee <narilee91@gmail.com>
2022-10-09 02:49:22 +0900

    Feat: 티켓 등록 폼 현재 단계 표시하는 진행바 추가
    - 작은 화면에서 해당 단계의 타이틀을 포함하게 되면,
    화면에 렌더링되는 폼의 가독성이 좋지 않아 태블릿 이상의 크기에서만 표시함.
    
    - 최대한 props를 넘기지 않고 동적인 스타일을 적용하기 위해 var 변수를 활용함.

---
+++ src/components/ProgressIndicator/index.tsx
@@ -0,0 +1,38 @@
+import { colors } from '@styles/theme';
+import { styles } from './styles';
+
+interface Props {
+  step: number;
+  stepTitles: string[];
+}
+
+export default function ProgressIndicator({ step, stepTitles }: Props) {
+  const progressWidth =
+    step == stepTitles.length
+      ? '100%'
+      : `${(step - 1) * (100 / (stepTitles.length - 1))}%`;
+
+  return (
+    <div css={styles.progressIndicator}>
+      <div
+        css={styles.progressBar}
+        style={{ '--progress-width': progressWidth }}
+      />
+      {stepTitles.map((title, index) => (
+        <div
+          key={title}
+          css={styles.progressBullet}
+          style={{
+            '--bullet-bg':
+              index + 1 < step ? colors.indigo[300] : colors.indigo[50],
+            '--bullet-border':
+              index + 1 <= step
+                ? `2px solid ${colors.indigo[300]}`
+                : '2px solid transparent',
+          }}
+          data-title={title}
+        />
+      ))}
+    </div>
+  );
+}
