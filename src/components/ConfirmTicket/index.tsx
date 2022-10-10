d8e168fca2174f3e2b76ed013a87092410b8c4b3
Nari Lee <narilee91@gmail.com>
2022-10-10 13:29:20 +0900

    Feat: 사용자가 등록한 티켓 마지막 확인 단계 추가

---
+++ src/components/ConfirmTicket/index.tsx
@@ -0,0 +1,48 @@
+import { KBO_LEAGUE_TEAMS } from '@constants/global';
+import { useTicketForm } from '@context/TicketFormContext';
+import { styles } from './styles';
+
+const weekdays = ['일', '월', '화', '수', '목', '금', '토'];
+
+export default function ConfirmTicket() {
+  const {
+    matchDate: { year, month, date },
+    homeTeam,
+    awayTeam,
+    stadium,
+    score,
+    scoreType,
+  } = useTicketForm();
+  const matchDate = `${year}.${month}.${date}`;
+  const matchDay = weekdays[new Date(year, month - 1, date).getDay()];
+  const matchup = `${awayTeam && KBO_LEAGUE_TEAMS[awayTeam]} vs ${
+    homeTeam && KBO_LEAGUE_TEAMS[homeTeam]
+  }`;
+
+  return (
+    <div>
+      <h3>티켓 정보</h3>
+      <div css={styles.ticketWrapper}>
+        <div css={styles.ticket}>
+          <p css={styles.title}>{year} KBO 리그</p>
+          <p css={styles.score}>
+            <span>
+              {score.awayTeam} : {score.homeTeam}
+            </span>
+            <em>{scoreType}</em>
+          </p>
+          <p
+            css={styles.matchup}
+            style={{ '--home-logo': `url('images/team/${homeTeam}.png')` }}
+          >
+            {matchup}
+          </p>
+          <p css={styles.date}>
+            {matchDate} ({matchDay})
+          </p>
+          <p css={styles.stadium}>{stadium}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
