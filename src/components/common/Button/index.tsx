51d7510832fb6fbdf7eca2eb224ae50f890717ec
Nari Lee <narilee91@gmail.com>
2022-10-10 17:49:53 +0900

    Refactor: 버튼 사이즈 조정 var 변수 대신 css prop 사용

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -5,23 +5,8 @@ import { styles } from './styles';
 interface Props {
   fullWidth?: boolean;
   bgColor?: string;
-  size?: keyof typeof sizes;
+  size?: 'small' | 'medium' | 'large';
 }
-
-const sizes = {
-  small: {
-    height: '1.5rem',
-    fontSize: '.875rem',
-  },
-  medium: {
-    height: '2.25rem',
-    fontSize: '1rem',
-  },
-  large: {
-    height: '3rem',
-    fontSize: '1.25rem',
-  },
-} as const;
 
 export default function Button({
   children,
@@ -33,11 +18,9 @@ export default function Button({
   return (
     <button
       type="button"
-      css={styles.button}
+      css={[styles.button, styles[size]]}
       style={{
         ['--button-width']: fullWidth ? '100%' : 'auto',
-        ['--button-height']: sizes[size].height,
-        ['--button-fontSize']: sizes[size].fontSize,
         ['--button-color']: bgColor ? colors.white : colors.black,
         ['--button-bg']: bgColor || 'transparent',
       }}
--- src/components/common/Button/styles.ts
+++ src/components/common/Button/styles.ts
@@ -3,16 +3,26 @@ import { css } from '@emotion/react';
 export const styles = {
   button: css({
     width: 'var(--button-width)',
-    height: 'var(--button-height)',
     paddingLeft: '1rem',
     paddingRight: '1rem',
     borderRadius: 4,
     color: 'var(--button-color)',
     backgroundColor: 'var(--button-bg)',
     fontWeight: 600,
-    fontSize: 'var(--button-fontSize)',
     '&:disabled': {
       cursor: 'not-allowed',
     },
+  }),
+  small: css({
+    height: '1.5rem',
+    fontSize: '.875rem',
+  }),
+  medium: css({
+    height: '2.25rem',
+    fontSize: '1rem',
+  }),
+  large: css({
+    height: '3rem',
+    fontSize: '1.25rem',
   }),
 };
