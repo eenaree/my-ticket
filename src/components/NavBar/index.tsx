65f3a6332fbbdb622459886f8c7718dc545839cc
Nari Lee <narilee91@gmail.com>
2022-10-11 21:33:30 +0900

    Feat(login): 인증 필요한 페이지에 로그인 여부 확인하는 라우터 추가 (#77)

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,50 +1,25 @@
 import Button from '@components/common/Button';
 import Modal from '@components/common/Modal';
 import StadiumPicker from '@components/StadiumPicker';
-import Login from '@pages/Login';
 import { useModalStore } from '@store/useModalStore';
 import { useUserStore } from '@store/useUserStore';
-import { User } from '@typings/db';
 import { styles } from './styles';
 
 export default function NavBar() {
   const modal = useModalStore(state => state.modal);
   const openModal = useModalStore(state => state.openModal);
-  const closeModal = useModalStore(state => state.closeModal);
-  const user = useUserStore(state => state.user);
-  const loginUser = useUserStore(state => state.loginUser);
   const logoutUser = useUserStore(state => state.logoutUser);
-
-  function onClickLogin() {
-    openModal('login');
-  }
-
-  function successLogin(user: User) {
-    loginUser(user);
-    closeModal();
-  }
-
-  function onClickTicketRegister() {
-    user ? openModal('stadium-picker') : openModal('login');
-  }
 
   return (
     <nav>
       <ul css={styles.navList}>
         <li>
-          <Button onClick={onClickTicketRegister}>티켓 등록</Button>
+          <Button onClick={() => openModal('stadium-picker')}>티켓 등록</Button>
         </li>
         <li>
-          {user ? (
-            <Button onClick={logoutUser}>로그아웃</Button>
-          ) : (
-            <Button onClick={onClickLogin}>로그인</Button>
-          )}
+          <Button onClick={logoutUser}>로그아웃</Button>
         </li>
       </ul>
-      <Modal modal={modal === 'login'}>
-        <Login onSuccess={successLogin} />
-      </Modal>
       <Modal modal={modal === 'stadium-picker'}>
         <StadiumPicker />
       </Modal>
---
+++ src/pages/RequireAuth.tsx
@@ -0,0 +1,7 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { useUserStore } from '@store/.';
+
+export default function RequireAuth() {
+  const { user } = useUserStore();
+  return user ? <Outlet /> : <Navigate to="login" replace />;
+}
