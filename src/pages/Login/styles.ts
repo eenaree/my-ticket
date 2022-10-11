e6b62820eadd576c686ba6286d2d0affb1c3417b
Nari Lee <narilee91@gmail.com>
2022-10-11 18:01:43 +0900

    Design: 로그인 컴포넌트 모달 디자인 제거 후 css 스타일 변경

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,72 +1,61 @@
 import { css } from '@emotion/react';
-import { mq } from '@styles/mediaQueries';
 import { colors } from '@styles/theme';
 
 export const styles = {
-  modalWrapper: css({
+  wrapper: css({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100vw',
+    minHeight: '100vh',
+    background: colors.gray[100],
+  }),
+  loginBox: css({
     display: 'flex',
     flexDirection: 'column',
+    justifyContent: 'space-evenly',
     width: '100%',
-    margin: 'auto 0.5rem',
-    backgroundColor: colors.white,
+    maxWidth: 360,
+    height: 400,
+    margin: '0 1rem',
+    padding: '1rem',
+    background: colors.white,
     borderRadius: 4,
-    [mq('xs')]: {
-      width: 360,
-      margin: 'auto',
-      maxHeight: 'calc(100% - 4rem)',
-    },
+    boxShadow: '0 2px 6px rgba(0,0,0,0.1)',
   }),
-  modalHeader: css({
+  logo: css({
+    height: 100,
     display: 'flex',
-    justifyContent: 'space-between',
-    backgroundColor: colors.gray[300],
-    borderRadius: '4px 4px 0 0',
-    padding: '1rem',
-    h2: {
-      fontFamily: 'UtoBalsamTint',
-      fontWeight: 600,
-      fontSize: '1.6rem',
-    },
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontFamily: 'UtoBalsamTint, Pretendard, sans-serif',
+    fontSize: '3rem',
+    fontWeight: 600,
   }),
-  closeButton: css({
-    position: 'relative',
-    height: 'auto',
-    '&:before': {
-      content: '""',
-      display: 'block',
-      position: 'absolute',
-      width: 3,
-      height: 20,
-      backgroundColor: colors.black,
-      transform: 'rotate(-45deg)',
-    },
-    '&:after': {
-      content: '""',
-      display: 'block',
-      width: 3,
-      height: 20,
-      backgroundColor: colors.black,
-      transform: 'rotate(45deg)',
-    },
-  }),
-  modalBody: css({
-    padding: '1rem',
-  }),
-  providerList: css({
-    li: {
-      margin: '1.25rem 0',
-      borderRadius: 4,
-    },
+  loginProvider: css({
+    margin: '1rem 0',
     button: {
       display: 'flex',
       alignItems: 'center',
       width: '100%',
       padding: 0,
-      backgroundColor: 'var(--provider-bg)',
-      color: 'var(--provider-color)',
+      borderRadius: 4,
+      fontWeight: 600,
     },
     span: {
       flex: '1 1 auto',
     },
+  }),
+  google: css({
+    background: colors.gray[100],
+    color: colors.black,
+  }),
+  kakao: css({
+    background: '#fee500',
+    color: colors.black,
+  }),
+  naver: css({
+    background: '#03c75a',
+    color: colors.white,
   }),
 };
