c5e95fc772993f10f628ffde9166f924c98df052
Nari Lee <narilee91@gmail.com>
2022-10-11 21:20:05 +0900

    Refactor: 기존에 로그인 사용자 확인하는 코드 모두 제거

--- src/components/MyTeamList/index.tsx
+++ src/components/MyTeamList/index.tsx
@@ -1,19 +1,15 @@
 import { useEffect } from 'react';
-import { useTeamStore, useUserStore } from '@store/.';
+import { useTeamStore } from '@store/.';
 import { styles } from './styles';
 
 export default function MyTeamList() {
-  const user = useUserStore(state => state.user);
   const myTeams = useTeamStore(state => state.myTeams);
   const getMyTeams = useTeamStore(state => state.getMyTeams);
 
   useEffect(() => {
-    if (user) {
-      getMyTeams();
-    }
-  }, [user, getMyTeams]);
+    getMyTeams();
+  }, []);
 
-  if (!user) return <div css={styles.noneTeamList}>로그인이 필요합니다.</div>;
   if (myTeams.length == 0)
     return <div css={styles.noneTeamList}>선택한 팀이 없습니다.</div>;
 
