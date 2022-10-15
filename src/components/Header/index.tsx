11376191fd093131a8209c0a3e473f10abcf98d0
Nari Lee <narilee91@gmail.com>
2022-10-15 23:36:43 +0900

    Design: delete header nav bar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,17 @@
 import { Link } from 'react-router-dom';
-import NavBar from '@components/NavBar';
+import Button from '@components/common/Button';
+import { useUserStore } from '@store/.';
 import { styles } from './styles';
 
 export default function Header() {
+  const logout = useUserStore(state => state.logout);
   return (
     <header>
       <div css={styles.headerInner}>
         <h1 css={styles.logo}>
           <Link to="/">MyTicket</Link>
         </h1>
-        <NavBar />
+        <Button onClick={logout}>로그아웃</Button>
       </div>
     </header>
   );
