f8b5e877460f7303c5e137eb27b7cf3b5b54725f
Nari Lee <narilee91@gmail.com>
2022-10-17 23:50:15 +0900

    Feat(types): ticket api data type

--- src/typings/db.ts
+++ src/typings/db.ts
@@ -9,3 +9,28 @@ export interface User {
 
 export type TeamId = keyof typeof KBO_LEAGUE_TEAMS;
 export type TeamName = typeof KBO_LEAGUE_TEAMS[TeamId];
+
+export interface Season {
+  id: number;
+  season: string;
+}
+
+export interface Stadium {
+  id: number;
+  stadium: string;
+  TeamId: number;
+}
+
+export interface Ticket {
+  id: number;
+  date: string;
+  homeTeam: TeamId;
+  awayTeam: TeamId;
+  homeTeamScore: number;
+  awayTeamScore: number;
+  scoreType: '승' | '패' | '무';
+  myTeam: TeamId;
+  opponentTeam: TeamId;
+  Stadium: Stadium;
+  Seasons: Season[];
+}
