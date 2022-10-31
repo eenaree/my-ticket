cdd7b1e7ec67970feff0d8472f699ed6c70bd400
Nari Lee <narilee91@gmail.com>
2022-11-01 01:10:03 +0900

    Refactor(types): authServices response type

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,16 +1,10 @@
 import { User } from '~/typings/db';
 import { httpClient } from '.';
 
-interface AuthResponse {
-  success: boolean;
-  message: string;
-  user: User;
-}
-
 export function login() {
-  return httpClient.get<AuthResponse>('/auth/login');
+  return httpClient.get<{ user: User }>('/auth/login');
 }
 
 export function logout() {
-  return httpClient.post<Omit<AuthResponse, 'user'>>('/auth/logout');
+  return httpClient.post('/auth/logout');
 }
